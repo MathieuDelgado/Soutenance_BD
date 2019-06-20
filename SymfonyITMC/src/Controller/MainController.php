720b8e87631c8917484aef7ea6f4e0dac508297a
Romain <romain.jacques.rj@gmail.com>
2019-06-20 14:03:07 +0200

    vu Accueil et inscription ok, base fonctionnelle

---
+++ SymfonyITMC/src/Controller/MainController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use App\Entity\User;
+use App\Entity\Book;
+use App\Entity\Kind;
+
+use App\Service\Recaptcha;
+
+class MainController extends AbstractController
+{
+
+    /**
+     * @Route("/Accueil/", name="home")
+     * Page d'accueil du site
+     */
+     public function home()
+     {
+         return $this->render('home.html.twig');
+     }
+
+    /**
+     * @Route("/inscrivez-vous/", name="register")
+     * Page d'inscription
+     */
+     public function register()
+     {
+
+        return $this->render('register.html.twig');
+     }
+
+}
