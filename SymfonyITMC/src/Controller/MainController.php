957b11ae1f30dc8b00382cd827b5ef75566d1032
Romain <romain.jacques.rj@gmail.com>
2019-06-21 16:38:34 +0200

    deconnexion valide

--- SymfonyITMC/src/Controller/MainController.php
+++ SymfonyITMC/src/Controller/MainController.php
@@ -177,7 +177,6 @@ class MainController extends AbstractController
 
     }
 
-<<<<<<< HEAD
     /**
      * @Route("/connexion/", name="login")
      * Page de connexion
@@ -273,21 +272,3 @@ class MainController extends AbstractController
 
 }
 
-=======
-
-    /**
-     * @Route("/detail-de-la-BD/{idBook}/", requirements={"name"="[1-9][0-9]{0,10}"}, name="displayOneBD")
-     * Page détail d'une seule BD
-     */
-    public function displayOneBD($idBook)
-    {
-        //via le repository des Book, on récupère la BD qui correspond à book_id dans l'url
-        $bookRepo = $this->getDoctrine()->getRepository(Book::class);
-        $book = $bookRepo->findOneById($idBook);
-
-             return $this->render('displayOneBD.html.twig', array(
-            'book'=>$book
-             ));            
-    }
-}
->>>>>>> origin/master
